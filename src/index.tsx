2b3ceea01faf5329d4bc2c8e53219edd0059eb45
tamakiii <tamakiii@users.noreply.github.com>
2019-04-02 15:28:57 +0900

    on componentDiMount() works

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,17 @@ import React, { useState, useCallback, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = "./src/alert.js";
+    document.getElementById('zucks_home').replaceWith(script)
+  }
+
   render() {
     return (
       <>
         <h1>hello</h1>
-        <script src="./src/alert.js"></script>
+        <div id="zucks_home"></div>
       </>
     )
   }
